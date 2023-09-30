048c04ef06249f022611e3433a8b48c39fb47620
Çağrı Sakaoğlu <cagrisakaoglu@gmail.com>
2023-09-30 13:09:58 +0100

    US-18 ApplicationUserRole added

--- URLShortener.Shared/Entities/User.cs
+++ URLShortener.Shared/Entities/User.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using URLShortener.Shared.Identity;
+
 namespace URLShortener.Shared.Entities;
 public class User : IdentityUser<Guid> 
 {
     public List<Link> Links { get; set; }
+    public ApplicationRole ApplicationRole { get; set; }
 }
-
