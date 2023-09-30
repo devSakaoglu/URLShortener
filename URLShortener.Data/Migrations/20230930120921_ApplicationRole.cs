75817dc6feb4150033e7a3cdd5c9b6e681636cde
Çağrı Sakaoğlu <cagrisakaoglu@gmail.com>
2023-09-30 13:09:58 +0100

    US-18 ApplicationUserRole added

---
+++ URLShortener.Data/Migrations/20230930120921_ApplicationRole.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ApplicationRoleId",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ApplicationRoleId",
+                table: "AspNetUsers",
+                column: "ApplicationRoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_ApplicationRoleId",
+                table: "AspNetUsers",
+                column: "ApplicationRoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_ApplicationRoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ApplicationRoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationRoleId",
+                table: "AspNetUsers");
+        }
+    }
+}
